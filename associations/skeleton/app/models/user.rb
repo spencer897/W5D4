2f3b618cc61abe27a7fad22d0d70c91bfb8525ac
anicholas4747 <an1720@nyu.edu>
2019-12-19 17:05:26 -0500

    W5D4 before url project

---
+++ associations/skeleton/app/models/user.rb
@@ -0,0 +1,26 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id         :bigint           not null, primary key
+#  name       :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
+class User < ApplicationRecord
+    has_many :enrollments,
+        class_name: "Enrollment",
+        primary_key: :id,
+        foreign_key: :student_id 
+
+    has_many :enrolled_courses,
+        through: :enrollments,
+        source: :course
+
+    has_one :course_to_teach,
+        class_name: "Course",
+        primary_key: :id,
+        foreign_key: instructor_id
+
+end
